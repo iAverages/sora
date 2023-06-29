ba0828da6acee69327e9e5c1e992974dcc316174
iAverages <31937542+iAverages@users.noreply.github.com>
2023-06-30 00:59:53 +0100

    setting up shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+// import { getServerAuthSession } from "~/server/auth";
+
 const Index = () => {
+    // const data = await getServerAuthSession();
+
     return (
         <div>
             <h1>Index</h1>
+            {/* <p>{JSON.stringify(data)}</p> */}
         </div>
     );
 };
---
+++ src/app/what/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function HUH() {
+    return (
+        <div>
+            <h1>Index</h1>
+        </div>
+    );
+}
