b1dde908504db65ece6492986b513100cb93a32b
iAverages <31937542+iAverages@users.noreply.github.com>
2023-06-29 23:07:56 +0100

    Cleanup route callback

--- src/app/api/callback/[userid]/route.ts
+++ src/app/api/callback/[userid]/route.ts
@@ -3,15 +3,13 @@ import { NextResponse } from "next/server";
 import { prisma } from "~/server/db";
 
 export const POST = async (request: Request, { params }: { params: { userid: string } }) => {
-    const { headers } = request;
-    const { userid } = params;
-    const body = await request.text();
     try {
+        const body = await request.text();
         const record = await prisma.requestBin.create({
             data: {
                 body: body,
-                headers: Array.from(headers).join(""),
-                userId: userid,
+                headers: Array.from(request.headers).join(""),
+                userId: params.userid,
             },
         });
 
@@ -19,7 +17,9 @@ export const POST = async (request: Request, { params }: { params: { userid: str
     } catch (error) {
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
             // "Foreign key constraint failed"
-            if (error.code === "P2003") return NextResponse.json({ error: "An error has occured" }, { status: 500 });
+            if (error.code === "P2003") {
+                return NextResponse.json({ error: "No callback route found." }, { status: 500 });
+            }
         }
         return NextResponse.json({ error: "An error has occured" }, { status: 500 });
     }
